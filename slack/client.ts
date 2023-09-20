7aaeb592002f13a37375c3fd66122fdcde2c6850
mactkg <mactkg@gmail.com>
2023-09-20 21:53:29 +0900

    Update deno_slack_api

--- slack/client.ts
+++ slack/client.ts
@@ -1,5 +1,5 @@
 import { SocketModeClient } from "https://deno.land/x/slack_socket_mode@1.0.3/mod.ts";
-import { SlackAPI } from "https://deno.land/x/deno_slack_api@1.5.0/mod.ts";
+import { SlackAPI } from "https://deno.land/x/deno_slack_api@2.1.2/mod.ts";
 
 const socketToken = Deno.env.get("SLACK_SOCKET_TOKEN");
 if (!socketToken) Deno.exit(1);
